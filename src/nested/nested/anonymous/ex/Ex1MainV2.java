e6cdb04d3efc3381afea0a99aef12b3ec08d828a
yslee <leeys9423@gmail.com>
2024-07-15 22:30:07 +0900

    익명 클래스 활용3

---
+++ src/nested/nested/anonymous/ex/Ex1MainV2.java
@@ -0,0 +1,45 @@
+package nested.nested.anonymous.ex;
+
+import java.util.Random;
+
+public class Ex1MainV2 {
+
+    public static void hello(Process process) {
+        System.out.println("프로그램 시작");
+
+        // 코드 조각 시작
+        process.run();
+        // 코드 조각 종료
+
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+
+        class Dice implements Process {
+
+            @Override
+            public void run() {
+                int randomValue = new Random().nextInt(6) + 1;
+                System.out.println("randomValue = " + randomValue);
+            }
+
+        }
+
+         class Sum implements Process {
+
+            @Override
+            public void run() {
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("i = " + i);
+                }
+            }
+
+        }
+
+        Dice dice = new Dice();
+        Sum sum = new Sum();
+        hello(dice);
+        hello(sum);
+    }
+}
---
+++ src/nested/nested/anonymous/ex/Ex1MainV4.java
@@ -0,0 +1,35 @@
+package nested.nested.anonymous.ex;
+
+import java.util.Random;
+
+public class Ex1MainV4 {
+
+    public static void hello(Process process) {
+        System.out.println("프로그램 시작");
+
+        // 코드 조각 시작
+        process.run();
+        // 코드 조각 종료
+
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+
+        hello(new Process() {
+            @Override
+            public void run() {
+                int randomValue = new Random().nextInt(6) + 1;
+                System.out.println("randomValue = " + randomValue);
+            }
+        });
+        hello(new Process() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("i = " + i);
+                }
+            }
+        });
+    }
+}
