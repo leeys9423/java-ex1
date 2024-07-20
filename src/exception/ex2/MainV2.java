e679a411234a870b586cf33b6f5dcbbfc2636471
yslee <leeys9423@gmail.com>
2024-07-21 00:44:09 +0900

    예외 처리 도입2 - 예외 복구

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -6,7 +6,8 @@ public class MainV2 {
 
     public static void main(String[] args) throws NetworkClientExceptionV2 {
 
-        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        //NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
